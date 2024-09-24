2a6a3c38cb911dbae320083b745ab209f935ba6b
Kiran Kumar <kirankumarpilli82gmail.com>
2024-09-24 19:44:58 +0530

    hii

---
+++ kk.js
@@ -0,0 +1,20 @@
+ // find which number is greatest among three numbers
+
+ let num1=20
+ let num2=30
+ let num3=24
+ if(num1>=num2||num1>=num3){
+     console.log("number one is greatest");
+     
+ }else if(num2>=num1||num2>=num3){
+    console.log("number two is greatest");
+    
+ }else{
+     console.log("number three is greatest");
+     
+ }
+ // 2.print numbers that will countdown 10-1
+ for(i=10;i>=1;i--){
+     console.log(i);
+     
+ }
